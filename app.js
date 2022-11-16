0ac5654b3dd3e18f645d577dec5cc96d77fe1ca8
NickG35 <ngeorge356@gmail.com>
2022-11-16 15:07:45 -0500

    adding the results to each input

--- app.js
+++ app.js
@@ -3,3 +3,22 @@ function getComputerChoice(){
     let randomChoice = rps[Math.floor(Math.random()*rps.length)];
     return randomChoice;
 }
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == computerSelection) {
+        return ("It is a tie!");
+    } else if ((playerSelection == "rock") && (computerSelection == "scissors")) {
+        return ("You win! Rock defeats scissors!");
+    } else if ((playerSelection == "rock") && (computerSelection == "paper")) {
+        return ("You lose! Paper defeats rock!");
+    } else if ((playerSelection == "paper") && (computerSelection == "rock")) {
+        return ("You win! Paper defeats rock!");
+    } else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
+        return ("You lose! Scissors defeats paper!");
+    } else if ((playerSelection == "scissors") && (computerSelection == "rock")) {
+        return ("You lose! Rock defeats scissors!");
+    } else if ((playerSelection == "scissors") && (computerSelection == "paper")) {
+        return ("You win! Scissors defeats rock!");
+    }
+}
+
